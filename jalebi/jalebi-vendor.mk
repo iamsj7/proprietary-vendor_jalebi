a3fd69f9050186dcbd94f5674cd67fc6b3dee151
Shaik Jaleel <shaikjaleel270@gmail.com>
2018-09-29 16:31:17 +0530

    jalebi: add atX library
    Co-Authored-By: Harshit Jain <dev-harsh1998@users.noreply.github.com>

--- jalebi/jalebi-vendor.mk
+++ jalebi/jalebi-vendor.mk
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
     vendor/yu/jalebi/proprietary/vendor/etc/permissions/qcrilhook.xml:system/vendor/etc/permissions/qcrilhook.xml \
     vendor/yu/jalebi/proprietary/vendor/etc/permissions/qti_permissions.xml:system/vendor/etc/permissions/qti_permissions.xml \
     vendor/yu/jalebi/proprietary/vendor/bin/perfd:system/vendor/bin/perfd \
+	vendor/yu/jalebi/proprietary/lib/libaptX_encoder.so:system/lib/libaptX_encoder.so \
     vendor/yu/jalebi/proprietary/vendor/bin/thermal-engine:system/vendor/bin/thermal-engine \
     vendor/yu/jalebi/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:system/vendor/lib/egl/eglSubDriverAndroid.so \
     vendor/yu/jalebi/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
